326bcba0cc0520d32004c10cbd454b004e9d6311
KiriteeKarunya <kiritee@tatrasdata.com>
2018-06-12 17:24:52 +0530

    added recording and chunk write - wip unanticipated host error

--- app_main.py
+++ app_main.py
@@ -1,28 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
+from gevent.pywsgi import WSGIServer
+from gevent import monkey
 
 from audio_recoder import AudioRecorder
 
+# monkey.patch_all()
+
 app = Flask(__name__)
+
+reference_dict = dict()
 
 
 @app.route(rule="/")
 def index_page():
-    return "Audio recording page"
+    return "Welcome to live audio streaming"
 
 
-@app.route(rule="/start", methods=['POST'])
-def start_record():
+@app.route(rule="/audio/<type_>", methods=['GET', 'POST'])
+def start_record(type_):
     request_ = request.get_json()
-    podcast_name = request_['filename']
-    AudioRecorder(audio_file_name=podcast_name).start()
+    recording_name = request_['filename']
+    if type_ == 'start':
+        cls_ = AudioRecorder(audio_file_name=recording_name)
+        reference_dict[recording_name] = cls_
+        cls_.record()
+        return jsonify({"message": "Recording successfully started"})
+    elif type_ == 'stop':
+        print(reference_dict)
+        cls_ = reference_dict[recording_name]
+        cls_.stop()
+        return jsonify({"message": "Recording successfully stopped"})
+    else:
+        return jsonify({"message": "Unable to process get request"})
 
 
-@app.route(rule="/stop", methods=['POST'])
-def stop_record():
-    request_ = request.get_json()
-    podcast_name = request_['filename']
-    AudioRecorder(audio_file_name=podcast_name).stop()
+@app.route(rule="/seek", methods=['POST'])
+def seek_record():
+    # request_ = request.get_json()
+    # recording_name = request_['filename']
+    pass
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True, processes=1)
+    # http_ = WSGIServer(('', 5000), app.wsgi_app)
+    # http_.serve_forever()
--- audio_recoder.py
+++ audio_recoder.py
@@ -1,28 +1,51 @@
 import pyaudio
 import wave
 
+from time import time
+
 
 class AudioRecorder(object):
-    def __init__(self, audio_file_name):
+    """
+    Rate is number of samples per second
+    Chunk size is the amount of bytes a sample can contain
+    E.g: To record five seconds of audio at a rate of say 16000Hz, we will be having 80000 sample.
+    But each chunk can hold 1000 samples. So, if we have a loop with this,
+     the appended array will have 80000/1000 = 80 samples.
+    """
+
+    def __init__(self, audio_file_name, recording_chunk_size=4):
         self.open = True
-        self.rate = 44100
-        self.frames_per_buffer = 1024
-        self.channels = 2
-        self.format = pyaudio.paInt16
+        self.frames_per_buffer = 256
+        self.channels = 1
+        self.format = pyaudio.paInt8
         self.audio_filename = "{}.wav".format(audio_file_name)
+        self.folder_name = audio_file_name
         self.audio = pyaudio.PyAudio()
+        self.rate = int(self.audio.get_device_info_by_index(0)['defaultSampleRate'])
         self.stream = self.audio.open(format=self.format,
                                       channels=self.channels,
                                       rate=self.rate,
                                       input=True,
                                       frames_per_buffer=self.frames_per_buffer)
+        self.start_time = None
+        self.elapsed_time = None
+        self.chunks = 0
         self.audio_frames = []
+        self.recording_chunk_size = recording_chunk_size
 
     def record(self):
         self.stream.start_stream()
+        chunk_start_time = time()
         while self.open:
-            data = self.stream.read(self.frames_per_buffer)
+            data = self.stream.read(self.frames_per_buffer, exception_on_overflow=False)
             self.audio_frames.append(data)
+
+            if time()-chunk_start_time > self.recording_chunk_size:
+                self.chunks += 1
+                self._write_chunks()
+                self.audio_frames = []
+                chunk_start_time = time()
+
             if not self.open:
                 break
 
@@ -32,13 +55,24 @@ class AudioRecorder(object):
             self.stream.stop_stream()
             self.stream.close()
             self.audio.terminate()
-            wave_file = wave.open(self.audio_filename, 'wb')
-            wave_file.setnchannels(self.channels)
-            wave_file.setsampwidth(self.audio.get_sample_size(self.format))
-            wave_file.setframerate(self.rate)
-            wave_file.writeframes(b''.join(self.audio_frames))
-            wave_file.close()
+            self.elapsed_time = time()-self.start_time
+            self._write_chunks()
 
     # Launches the audio recording function using a thread
     def start(self):
         self.record().start()
+
+    def _write_chunks(self):
+        import os
+        if not os.path.exists("recordings/{}".format(self.folder_name)):
+            os.makedirs("recordings/{}".format(self.folder_name))
+
+        wavefile = wave.open("recordings/{}/chunk_{}.wav".format(self.folder_name, self.chunks), 'wb')
+        wavefile.setnchannels(nchannels=self.channels)
+        wavefile.setsampwidth(self.audio.get_sample_size(self.format))
+        wavefile.setframerate(self.rate)
+        wavefile.writeframes(b''.join(self.audio_frames))
+        wavefile.close()
+
+    def seek(self):
+        pass
